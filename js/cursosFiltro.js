2f8144674ba0194a00c9d56500d52c4a716b2625
ArmandoVn <armandoeloriginal@live.com.mx>
2017-05-04 12:59:34 -0500

    Se agrego el parallax a convocatoria y se modifico la navbar del index y convocatoria. Se agrego un pruba de angular para la busqueda de cartas.

---
+++ js/cursosFiltro.js
@@ -0,0 +1,40 @@
+function controladorFiltros($scope){
+	$scope.cursos = [
+	    {
+	      nombre:'C'
+	    },
+	    {
+	    	nombre: 'C++'
+	    },
+	    {
+	    	nombre: 'Redes y seguridad'
+	    },
+	    {
+	    	nombre: 'Computo forense'
+	    },
+	    {
+	    	nombre: 'Base de datos'
+	    },
+	    {
+	    	nombre: 'Linux'
+	    },
+	    {
+	    	nombre: 'Labview'
+	    },
+	    {
+	    	nombre: 'Desarrollo web'
+	    },
+	    {
+	    	nombre: 'Android'
+	    },
+	    {
+	    	nombre: 'DJango'
+	    },
+	    {
+	    	nombre: 'JAVA'
+	    },
+	    {
+	    	nombre: 'Fortran'
+	    }
+  	]
+    }
